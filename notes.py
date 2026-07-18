67bdfb42f00f19c6468a2158e408f1feee2668d7
andrea4ferraro <andreafrr@proton.me>
2026-07-18 23:12:05 +0300

    Update notes.py

--- notes.py
+++ notes.py
@@ -1,1 +1,14 @@
-class NoteApp: pass
+class Note:
+
+    def __init__(
+        self,
+        title,
+        text,
+        category
+    ):
+
+        self.title = title
+
+        self.text = text
+
+        self.category = category
