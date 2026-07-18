cfe61fefe9e94d75b3f4d844cb24d0108f36ce13
andrea4ferraro <andreafrr@proton.me>
2026-07-18 23:13:01 +0300

    Create sample_notes.py

---
+++ sample_notes.py
@@ -0,0 +1,35 @@
+from note import Note
+
+NOTES = [
+
+    Note(
+
+        "API",
+
+        "Finish API documentation",
+
+        "Work"
+
+    ),
+
+    Note(
+
+        "Shopping",
+
+        "Buy groceries",
+
+        "Personal"
+
+    ),
+
+    Note(
+
+        "Widget",
+
+        "Create a desktop weather widget",
+
+        "Ideas"
+
+    )
+
+]
